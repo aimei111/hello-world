9fc0ad047abf7fe0d6985b2423abaa84864eca82
Lgsc <LGSC@lgscdemac-mini.local>
2017-09-13 10:14:09 +0800

    测试提交

--- RunTimeTest/ViewController.m
+++ RunTimeTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <objc/message.h>
 
 @interface ViewController ()
 
@@ -17,7 +18,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    objc_msgSend(self, @selector(viewDidLoad));
+    
+//    [ViewController load];
+    
+    [ViewController xz_imageName:@"123"];
 }
+
++ (UIImage *)xz_imageName:(NSString *)imageName
+{
+    // 1.加载图片
+    UIImage *image = [ViewController xz_imageName:imageName];
+    
+    // 2.判断功能
+    if (image == nil) {
+        NSLog(@"%@图片为空",imageName);
+    }
+    
+    return image;
+}
+
++ (void)load
+{
+    NSLog(@"%s",__func__);
+    
+    // 1.表示获取方法的实现
+    //class_getMethodImplementation(<#__unsafe_unretained Class cls#>, <#SEL name#>)
+    // 2.获取对象方法
+    //  class_getInstanceMethod(<#__unsafe_unretained Class cls#>, <#SEL name#>)
+    // 3.获取类方法
+    //  class_getClassMethod(<#__unsafe_unretained Class cls#>, <#SEL name#>)
+    
+    /**
+     class:获取哪个类的方法
+     sel:获取方法编号，根据sel去找相应的类方法
+     */
+    Method imageNameMethod =  class_getClassMethod([UIImage class], @selector(imageNamed:));
+    
+    Method xz_imageNameMethod = class_getClassMethod([UIImage class], @selector(xz_imageName:));
+    
+    // 交换方法实现
+    method_exchangeImplementations(imageNameMethod, xz_imageNameMethod);
+}
+
 
 
 - (void)didReceiveMemoryWarning {
